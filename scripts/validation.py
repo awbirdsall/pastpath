f62b11755886d0e9fe303beda144c690d5060ade
awbirdsall <abirdsall@gmail.com>
2019-01-30 23:25:54 -0500

    rework validation script
    - use latest data formats

--- scripts/validation.py
+++ scripts/validation.py
@@ -6,11 +6,11 @@
 import numpy as np
 import pandas as pd
 import random
+from sys import argv
 
-MARKER_CSV = '../data/190121-all-markers-with-cats.csv'
-ENT_CSV = "../data/190128-df-ent.csv"
-SIM_TFIDF_CSV = "../data/190128-df-sim-tfidf.csv"
-SIM_CSV = "../data/190128-df-sim.csv"
+MARKER_CSV = '../data/190130-df-marker.csv'
+FEAT_CSV = "../data/190130-df-feature-counts.csv"
+SIM_CSV = "../data/190130-df-sim-tfidf.csv"
 
 def top_similar_marker_id(marker_id, sim_csv, n=10):
     df_sim = pd.read_csv(sim_csv, index_col='marker_id')
@@ -28,21 +28,34 @@ def top_similarities_random(marker_csv, sim_csv, n=10):
     random_id = random.choice(df.marker_id.unique())
     return top_similar_marker_id(random_id, sim_csv, n)
 
+def marker_title_from_id(marker_id, marker_csv):
+    df = pd.read_csv(marker_csv).query("cty=='washington_dc'")
+    marker = df[df.marker_id==marker_id]
+    return marker.title.values[0]
+
 def marker_text_from_id(marker_id, marker_csv):
     df = pd.read_csv(marker_csv).query("cty=='washington_dc'")
     marker = df[df.marker_id==marker_id]
-    return marker.text.values[0]
+    return marker.text_clean.values[0]
 
-def marker_ents_from_id(marker_id, ent_csv):
-    df_ent = pd.read_csv(ent_csv)
-    marker = df_ent[df_ent.marker_id==marker_id]
-    marker_ents = marker.text.tolist()
-    return marker_ents
+def marker_feats_from_id(marker_id, feat_csv):
+    df_feat = pd.read_csv(feat_csv, index_col='marker_id')
+    marker_has_feat = df_feat.loc[marker_id]
+    marker_feats = df_feat.columns[marker_has_feat].tolist()
+    return marker_feats
 
-def marker_cats_from_id(marker_id, marker_csv):
-    df = pd.read_csv(marker_csv).query("cty=='washington_dc'")
-    marker = df[df.marker_id==marker_id]
-    return marker.categories.values[0]
+def short_text_report(top_n_id, top_n_sims):
+    primary_id = top_n_id[0]
+    similar_ids = top_n_id[1:]
+    print("primary: {} (#{})".format(marker_title_from_id(primary_id, MARKER_CSV),
+                                     primary_id))
+    print("similar:")
+    for similar_id in similar_ids:
+        print("{} (#{})".format(marker_title_from_id(similar_id, MARKER_CSV),
+                                     similar_id))
+    print("scores: {}".format(top_n_sims))
+
+    return None
 
 def detailed_text_report(top_n_id, top_n_sims):
     primary_id = top_n_id[0]
@@ -51,19 +64,12 @@ def detailed_text_report(top_n_id, top_n_sims):
     print("top similar ids: {}".format(similar_ids))
     print("similarity scores: {}".format(top_n_sims))
     print('+++')
-    print('ENT COMPARISON')
-    print('primary m{} ents:'.format(primary_id))
-    print(marker_ents_from_id(primary_id, ENT_CSV))
+    print('FEAT COMPARISON')
+    print('primary m{} feats:'.format(primary_id))
+    print(marker_feats_from_id(primary_id, FEAT_CSV))
     for mid in similar_ids:
         print('---')
-        print("m{} entities: {}".format(mid, marker_ents_from_id(mid, ENT_CSV)))
-    print('+++')
-    print("CAT COMPARISON")
-    print('primary m{} cats:'.format(primary_id))
-    print(marker_cats_from_id(primary_id, MARKER_CSV))
-    for mid in similar_ids:
-        print('---')
-        print('m{} categories: {}'.format(mid, marker_cats_from_id(mid, MARKER_CSV)))
+        print("m{} feats: {}".format(mid, marker_feats_from_id(mid, FEAT_CSV)))
     print('+++')
     print("RAW TEXT COMPARISON")
     print("primary m{} text: {}".format(primary_id, marker_text_from_id(primary_id, MARKER_CSV)))
@@ -75,14 +81,31 @@ def detailed_text_report(top_n_id, top_n_sims):
 
 if __name__ == '__main__':
     print("running validation.py from command line")
+    print('MARKER_CSV = {}'.format(MARKER_CSV))
+    print('FEAT_CSV = {}'.format(FEAT_CSV))
+    print('SIM_CSV = {}'.format(SIM_CSV))
+
     print("======")
     print("RANDOM MARKER")
     print("======")
-    top_n_id, top_n_sims = top_similarities_random(MARKER_CSV, SIM_TFIDF_CSV, n=10)
+    top_n_id, top_n_sims = top_similarities_random(MARKER_CSV, SIM_CSV, n=10)
+    short_text_report(top_n_id, top_n_sims)
     detailed_text_report(top_n_id, top_n_sims)
 
     print("======")
     print("BARTHOLDI FOUNTAIN MARKER")
     print("======")
-    top_n_id, top_n_sims = top_similar_marker_id(110441, SIM_TFIDF_CSV, n=10)
-    detailed_text_report(top_n_id, top_n_sims)
+    top_n_id, top_n_sims = top_similar_marker_id(110441, SIM_CSV, n=10)
+    short_text_report(top_n_id, top_n_sims)
+
+    print("=====")
+    print("FREDERICK DOUGLAS MARKER")
+    print("=====")
+    top_n_id, top_n_sims = top_similar_marker_id(40846, SIM_CSV, n=10)
+    short_text_report(top_n_id, top_n_sims)
+
+    print("=====")
+    print("MARY CHURCH TERRELL MARKER")
+    print("=====")
+    top_n_id, top_n_sims = top_similar_marker_id(100863, SIM_CSV, n=10)
+    short_text_report(top_n_id, top_n_sims)
