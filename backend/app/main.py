f9557a4d161b89eb348dcaa1db9f703bf78be55a
awbirdsall <abirdsall@gmail.com>
2021-04-03 15:54:15 -0400

    Don't need corsmiddleware or static in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,35 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 import uvicorn 
 
 from app import views
 
 app = FastAPI(root_path="/api/v1")
-# TODO does this get served out of nginx or not
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-origins = [
-    # TODO handle CORS appropriately, if needed
-    "http://web",
-    "http://web:8080",
-    "127.18.0.3",
-    "127.18.0.3:8080",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://pastpath.tours",
-    "*"
-]
 
 app.include_router(views.router)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
 
 
 if __name__=='__main__':
